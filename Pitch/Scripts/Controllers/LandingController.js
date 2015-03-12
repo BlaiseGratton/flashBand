4638030a457d13f761ca11bc5bc1995e703a9312
BlaiseGratton <blaisegratton@gmail.com>
2015-03-12 16:53:41 -0500

    add players model class

---
+++ Pitch/Scripts/Controllers/LandingController.js
@@ -0,0 +1,12 @@
+﻿;(function(){
+    'use strict';
+    
+    angular.module('pitchApp')
+        .controller('LandingController', function($http) {
+
+            var vm = this;
+
+            vm.testModel = "stringstring";
+
+        })
+}());
