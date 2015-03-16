a08338e109c6ace3516f3f4a252593a88c9cc70a
BlaiseGratton <blaisegratton@gmail.com>
2015-03-16 11:59:45 -0500

    auth controllers and oAuth installed

--- Pitch/Startup.cs
+++ Pitch/Startup.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
 using Owin;
+using Pitch.Providers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,9 +15,26 @@ namespace Pitch
     {
         public void Configuration(IAppBuilder app)
         {
+            ConfigureOAuth(app);
             HttpConfiguration config = new HttpConfiguration();
             WebApiConfig.Register(config);
             app.UseWebApi(config);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
         }
     }
 }
