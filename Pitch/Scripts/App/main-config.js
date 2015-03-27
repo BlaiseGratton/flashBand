b7e4fac530c1450c2053181e5ed8f40b2cb367a7
BlaiseGratton <blaisegratton@gmail.com>
2015-03-27 15:53:37 -0500

    cleaned out junk code

--- Pitch/Scripts/App/main-config.js
+++ Pitch/Scripts/App/main-config.js
@@ -8,34 +8,22 @@ angular.module('pitchApp')
             controller: 'ProfileController',
             controllerAs: 'profCtrl'
         })
-        /*.when('/prof/edit', {
-            templateUrl: '/Static/Views/edit-profile.html',
-            controller: 'EditProfileController',
-            controllerAs: 'profCtrl',
-        })*/
         .when('/signup', {
             templateUrl: '/Static/Views/signup.html',
             controller: 'SignupController'
-            //controllerAs: 'signCtrl'
         })
         .when('/login', {
             templateUrl: '/Static/Views/login.html',
             controller: 'LoginController'
-            //controllerAs: 'loginCtrl'
         })
         .when('/home', {
             templateUrl: '/Static/Views/home.html',
             controller: 'HomeController'
-            //controllerAs: 'homeCtrl'
         })
         .when('/flashes', {
             templateUrl: '/Static/Views/flashes.html',
             controller: 'FlashesController'
         })
-        /*.when('/', {
-            templateUrl: '/Static/Views/home.html',
-            controller: 'IndexController',
-        })*/
         .otherwise({ redirectTo: '/home' });
     })
     .config(function ($httpProvider) {
